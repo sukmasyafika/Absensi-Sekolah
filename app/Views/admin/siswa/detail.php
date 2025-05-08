c58c7cb28cf68c4f0674aea630e342a2a90d29ce
fani <fanigiya@gmail.com>
2025-05-08 21:47:36 +0900

    detail baru

--- app/Views/admin/siswa/detail.php
+++ app/Views/admin/siswa/detail.php
@@ -10,24 +10,37 @@
         <i class="bi bi-house-door-fill"></i> Home
       </a>
     </li>
-    <li class="breadcrumb-item" aria-current="page"><a href="<?= base_url('siswa'); ?>" class="text-decoration-none">Siswa</a></li>
+    <li class="breadcrumb-item"><a href="<?= base_url('siswa'); ?>" class="text-decoration-none">Siswa</a></li>
     <li class="breadcrumb-item active" aria-current="page"><?= $title; ?></li>
   </ol>
 
-  <h1 class="h3 mb-3 text-gray-800 fw-bold"><?= $title ?></h1>
+  <h1 class="h3 mb-4 text-gray-800 fw-bold"><?= $title ?></h1>
 
-  <h3 class="text-uppercase">Nama lengkap <?= esc($siswa->nama); ?></h3>
-  <h3 class="text-uppercase">nisn: <?= esc($siswa->nisn); ?></h3>
-  <h3 class="text-uppercase">tanggal lahir: <?= esc($siswa->tanggal_lahir); ?></h3>
-  <h3 class="text-uppercase">jenis Kelamin: <?= esc($siswa->jenis_kelamin); ?></h3>
-  <h3 class="text-uppercase">Kelas: <?= esc($siswa->kelas_name . ' ' . $siswa->jurusan_name . ' ' . $siswa->rombel); ?></h3>
-  <h3 class="text-uppercase">agama: <?= esc($siswa->agama); ?></h3>
-  <h3 class="text-uppercase">tahun masuk: <?= esc($siswa->thn_masuk); ?></h3>
-  <h3 class="text-uppercase">status: <?= esc($siswa->status); ?></h3>
+  <div class="card shadow mb-4 border-0">
+    <div class="card-header py-3 bg-primary text-white">
+      <h5 class="m-0 fw-bold text-uppercase"><i class="bi bi-person-fill"></i> Detail Siswa</h5>
+    </div>
+    <div class="card-body">
+      <div class="row mb-3">
+        <div class="col-md-6">
+          <p><strong>Nama Lengkap:</strong> <?= esc($siswa->nama); ?></p>
+          <p><strong>NISN:</strong> <?= esc($siswa->nisn); ?></p>
+          <p><strong>Tanggal Lahir:</strong> <?= esc($siswa->tanggal_lahir); ?></p>
+          <p><strong>Jenis Kelamin:</strong> <?= esc($siswa->jenis_kelamin); ?></p>
+        </div>
+        <div class="col-md-6">
+          <p><strong>Kelas:</strong> <?= esc($siswa->kelas_name . ' ' . $siswa->jurusan_name . ' ' . $siswa->rombel); ?></p>
+          <p><strong>Agama:</strong> <?= esc($siswa->agama); ?></p>
+          <p><strong>Tahun Masuk:</strong> <?= esc($siswa->thn_masuk); ?></p>
+          <p><strong>Status:</strong> <?= esc($siswa->status); ?></p>
+        </div>
+      </div>
 
-  <a href="<?= base_url('/siswa'); ?>" class="btn btn-secondary me-3">
-    <i class="bi bi-arrow-left-square"></i> Kembali
-  </a>
+      <a href="<?= base_url('/siswa'); ?>" class="btn btn-secondary">
+        <i class="bi bi-arrow-left-square"></i> Kembali
+      </a>
+    </div>
+  </div>
 
 </div>
 
